46f8303e4975be1aac7ea72266b31b61d384895b
huy <nguyenminhhuy2905@gmail.com>
2025-05-09 18:52:29 +0700

    Get rid of cython for now

--- Pytorch/TinyVGG_test.py
+++ Pytorch/TinyVGG_test.py
@@ -1,12 +1,18 @@
-from models.Pytorch import TinyVGG
-from utils.ufuncs import train
+import sys
+import os
+# Add the project root directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from models.Pytorch.TinyVGG import TinyVGGv2
+from utils.ufuncs import train, plot_loss_curves, make_image_dataloader, make_image_dataset
 
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torchvision import transforms
 import os
+
+import matplotlib.pyplot as plt
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 train_path = 'data/pizza_steak_sushi_20_percent/train'
@@ -22,30 +28,17 @@ test_transformation = transforms.Compose([
     transforms.ToTensor()
 ])
 
-def make_dataset():
-    train_dataset = datasets.ImageFolder(
-        root=train_path,
-        transform=train_transformation,
-    )
-    test_dataset = datasets.ImageFolder(
-        root=test_path,
-        transform=test_transformation
-    )
-    return train_dataset, test_dataset
-
-def make_dataloader():
-    train_dataset, test_dataset = make_dataset()
-
-    train_dataloader = DataLoader(train_dataset, batch_size=32, num_workers=os.cpu_count(), shuffle=True)
-    test_dataloader = DataLoader(test_dataset, batch_size=32, num_workers=os.cpu_count(), shuffle=True)
-    return train_dataloader, test_dataloader
 
 if __name__ == '__main__':
-    train_dataloader, test_dataloader = make_dataloader()
+    train_dataset, test_dataset = make_image_dataset(train_path, train_transformation, test_path, test_transformation)
+    train_dataloader, test_dataloader = make_image_dataloader(train_dataset, test_dataset, num_workers=os.cpu_count(), batch_size=32, shuffle=True)
 
-    model = TinyVGG(1, 10, 3)
+    model = TinyVGGv2(3, 24, 3).to(device)
     lr = 0.001
-    optimizer = optim.Adam(model.parameters(), lr=lr)
+    optimizer = optim.SGD(model.parameters(), lr=lr)
     loss_fn = nn.CrossEntropyLoss()
 
-    train(model, train_dataloader, test_dataloader, optimizer, loss_fn, device, epochs=10, progress=False, verbose=True, every=2)
+    model_result = train(model, train_dataloader, test_dataloader, optimizer, loss_fn, device, epochs=30, progress=False, verbose=True, every=2)
+
+    plot_loss_curves(model_result)
+    plt.show()
